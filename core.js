98f7479818c39d496d5645598f706ec2c9f9ff6b
Pat Wilken <patrickwilken28@gmail.com>
2016-12-31 19:07:32 -0500

    Added user info
    - Shows username on: join, message, disconnect
    - Added "system" message type (full width)

--- core.js
+++ core.js
@@ -1,6 +1,7 @@
 var Core = require("express")();
 var HTTP = require("http").Server(Core);
 var IO = require("socket.io")(HTTP);
+var users = [];
 
 Core.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
@@ -9,12 +10,28 @@ Core.get("/", function(req, res){
 IO.on("connection", function(socket){
     console.log("a user connected");
     
+    socket.on("user joined", function(user){
+        users.push(user);
+        console.log("user " + user.nickname + " joined. ID: " + user.id);
+        IO.emit("user event", user.nickname + " joined");
+    });
+    
     socket.on("chat message", function(msg){
         IO.emit("chat message", msg);
     });
     
     socket.on("disconnect", function(){
-        console.log("user disconnected.");
+        var curUser = {};
+        for(var i=0; i<users.length; i++){
+            if(users[i].id == this.client.id){
+                curUser = users[i];
+                users.splice(i, 1);
+                break;
+            }
+        }
+        
+        console.log("user " + curUser.nickname + " disconnected.");
+        IO.emit("user event", curUser.nickname + " disconnected");
     });
 });
 
